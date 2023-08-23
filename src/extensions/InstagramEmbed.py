72216929f2f596b23d5a84dbe220016690a14b49
Fluxticks <benjigarment.appdev@gmail.com>
2023-08-23 19:20:52 +0200

    Updated InstagramEmbed to use new features

--- src/extensions/InstagramEmbed.py
+++ src/extensions/InstagramEmbed.py
@@ -1,21 +1,19 @@
-import asyncio
 import logging
 import os
 import re
+from urllib.parse import parse_qs, urlparse
 
-from discord import Embed, File, Interaction, Message, RawReactionActionEvent, PartialEmoji
+from discord import Embed, Interaction, Message, RawReactionActionEvent, PartialEmoji
 from discord.app_commands import command, default_permissions, describe, rename
 from discord.ext.commands import Bot, GroupCog
-from discord.utils import MISSING
-from instagramdl.exceptions import (InstagramInaccessibleException, PostUnavailableException)
+from instagramdl.exceptions import PostUnavailableException
 from instagramdl.get_post import get_info
 from instagramdl.post_data import InstagramPost
 
 from common.discord import respond_or_followup
-from common.io import load_cog_toml, stitch_videos, MAX_FILE_BYTES
+from common.io import load_cog_toml
 from database.gateway import DBSession
 from database.models import InstagramMessagesEnabled
-from common.io import reduce_video
 
 COG_STRINGS = load_cog_toml(__name__)
 REGEX_STR = r"https\:\/\/www\.instagram\.com\/(reel|p)\/[a-zA-Z0-9]+"
@@ -27,7 +25,42 @@ RETRY_EMOJI = PartialEmoji.from_str(os.getenv("INSTA_RETRY_EMOJI"))
 CONFIRM_EMOJI = PartialEmoji.from_str(os.getenv("INSTA_RESPONSE_EMOJI"))
 
 
-def embeds_from_post(post: InstagramPost) -> list[Embed]:
+def paginate_video_text(videos):
+    if len(videos) > 2000:
+        remaining = videos
+        output = []
+        while len(remaining) > 2000:
+            next_index = remaining.rfind(" [", 0, 2000)
+            output.append(remaining[:next_index])
+            remaining = remaining[next_index + 1:]
+        output.append(remaining)
+        return output
+    return [videos]
+
+
+def reduce_url_length(url: str):
+    needed_params = ["_nc_ht", "_nc_ohc", "edm", "oh", "oe"]
+    params = parse_qs(urlparse(url).query)
+    params_string = "&".join([f"{k}={v[0]}" for k, v in params.items() if k in needed_params])
+    base_string = url.split("?")[0]
+    return f"{base_string}?{params_string}"
+
+
+def parse_description(description: str):
+    tag_regex = r"(?<!\[)\#([0-9a-zA-Z])+(?!\])"
+    out_description = description
+    match = re.search(tag_regex, out_description, re.MULTILINE)
+    for _ in range(out_description.count("#")):
+        before = out_description[:match.start()]
+        after = out_description[match.end():]
+        string = out_description[match.start():match.end()]
+        replace_string = f"[{string}](https://www.instagram.com/explore/tags/{string.replace('#','')})"
+        out_description = before + replace_string + after
+        match = re.search(tag_regex, out_description, re.MULTILINE)
+    return out_description
+
+
+def make_embeds(post: InstagramPost):
     if len(post.post_description) > MAX_POST_DESCRIPTION_LENGTH:
         next_space = post.post_description.index(' ', MAX_POST_DESCRIPTION_LENGTH)
         if next_space == -1:
@@ -37,46 +70,32 @@ def embeds_from_post(post: InstagramPost) -> list[Embed]:
     else:
         description = post.post_description
 
-    main_embed = Embed(
-        title=f"@{post.author_display_name}'{'' if post.author_display_name.endswith('s') else 's'} Post",
-        description=description,
-        url=post.post_url,
-        color=INSTA_COLOUR
+    embed = Embed(
+        color=INSTA_COLOUR,
+        title=f"{'<a:verified:1143572299304407141> ' if post.author_is_verified else''}{post.author_display_name}'s Post",
+        description=parse_description(description),
+        url=post.post_url
     )
 
-    main_embed.set_author(name=f"@{post.author_username}", url=post.author_profile_url, icon_url=post.author_avatar_url)
-    main_embed.set_footer(
+    embed.set_author(name=f"@{post.author_username}", url=post.author_profile_url, icon_url=post.author_avatar_url)
+    embed.set_footer(
         text=f"Post on {post.post_timestamp.strftime('%d/%m/%Y')} at {post.post_timestamp.strftime('%-H:%M')}",
         icon_url=INSTA_ICON_URL
     )
 
-    if len(post.post_image_urls) == 1:
-        main_embed.set_image(url=post.post_image_urls[0])
-        return [main_embed]
+    embeds = [embed]
 
+    if not post.post_image_urls:
+        return embeds
+
+    embed.set_image(url=post.post_image_urls[0])
     if len(post.post_image_urls) > 1:
-        main_embed.set_image(url=post.post_image_urls[0])
-        embeds = [main_embed]
         for image in post.post_image_urls[1:]:
-            alt_embed = Embed(url=post.post_url)
-            alt_embed.set_image(url=image)
-            embeds.append(alt_embed)
-        return embeds
+            sub_embed = Embed(url=post.post_url)
+            sub_embed.set_image(url=image)
+            embeds.append(sub_embed)
 
-    if len(post.post_video_files) == 1:
-        main_embed.set_image(url=post.post_video_urls[0].get("thumbnail"))
-        return [main_embed]
-
-    if len(post.post_video_files) > 1:
-        main_embed.set_image(url=post.post_video_urls[0].get("thumbnail"))
-        embeds = [main_embed]
-        for video in post.post_video_urls[1:]:
-            alt_embed = Embed(url=post.post_url)
-            alt_embed.set_image(url=video.get("thumbnail"))
-            embeds.append(alt_embed)
-        return embeds
-
-    return [main_embed]
+    return embeds
 
 
 @default_permissions(administrator=True)
@@ -129,6 +148,36 @@ class InstagramEmbed(GroupCog, name=COG_STRINGS["instagram_group_name"]):
         self.logger = logging.getLogger(__name__)
         self.logger.info(f"{__name__} has been added as a Cog")
 
+    async def request_reply(self, url: str, interaction: Interaction = None, message: Message = None):
+
+        try:
+            post_data = await get_info(url, download_videos=False)
+        except PostUnavailableException:
+            if interaction:
+                await respond_or_followup(COG_STRINGS["instagram_warn_inaccessible"], interaction=interaction)
+            return False
+
+        embeds = make_embeds(post_data)
+        if post_data.post_video_urls:
+            videos = " ".join(
+                [f"[Video {idx + 1}]({reduce_url_length(x)})" for idx,
+                 x in enumerate(post_data.post_video_urls)]
+            )
+            paginated_videos = paginate_video_text(videos)
+        else:
+            paginated_videos = ""
+
+        if message:
+            new_message = await message.reply(embeds=embeds, mention_author=False)
+            for page in paginated_videos:
+                await new_message.reply(content=page, mention_author=False)
+        elif interaction:
+            await respond_or_followup(message="", embeds=embeds, interaction=interaction, delete_after=None)
+            for page in paginated_videos:
+                await respond_or_followup(message=page, interaction=interaction, delete_after=None)
+
+        return True
+
     @GroupCog.listener()
     async def on_message(self, message: Message):
         if message.author.bot:
@@ -138,9 +187,13 @@ class InstagramEmbed(GroupCog, name=COG_STRINGS["instagram_group_name"]):
         if not db_item or not db_item.is_enabled:
             return
 
+        should_suppress = False
         found_urls = re.finditer(REGEX_STR, message.content, re.MULTILINE)
         for _, match in enumerate(found_urls, start=1):
-            await self.post_response(url=match.string, message=message)
+            should_suppress = await self.request_reply(url=match.string, message=message) or should_suppress
+
+        if should_suppress:
+            await message.edit(suppress=True)
 
     @GroupCog.listener()
     async def on_raw_reaction_add(self, payload: RawReactionActionEvent):
@@ -163,65 +216,10 @@ class InstagramEmbed(GroupCog, name=COG_STRINGS["instagram_group_name"]):
 
         found_urls = re.finditer(REGEX_STR, message.content, re.MULTILINE)
         for _, match in enumerate(found_urls, start=1):
-            await self.post_response(url=match.string, message=message)
+            await self.request_reply(url=match.string, message=message)
 
         await message.remove_reaction(payload.emoji, payload.member)
         await message.remove_reaction(CONFIRM_EMOJI, guild.me)
-
-    async def post_response(self, url: str, message: Message = None, interaction: Interaction = None):
-
-        try:
-            post = await get_info(url)
-        except InstagramInaccessibleException as e:
-            if message:
-                return
-
-            await respond_or_followup(
-                message=COG_STRINGS["instagram_warn_inaccessible"].format(url=e.url),
-                interaction=interaction,
-                delete_after=10
-            )
-            return
-        except PostUnavailableException as e:
-            if message:
-                return
-
-            await respond_or_followup(
-                message=COG_STRINGS["instagram_warn_post_unavailable"].format(url=e.url),
-                interaction=interaction,
-                delete_after=10
-            )
-            return
-
-        embeds = embeds_from_post(post)
-
-        if post.post_video_files:
-            if len(post.post_video_files) > 1:
-                video = stitch_videos(post.post_video_files)
-                post.post_video_files.append(video)
-            else:
-                video = post.post_video_files[0]
-
-            file_size = os.path.getsize(video)
-            if file_size >= MAX_FILE_BYTES:
-                if interaction:
-                    await interaction.edit_original_response(content=COG_STRINGS["instagram_file_too_big"])
-                video = reduce_video(video)
-                post.post_video_files[-1] = video
-
-            attachments = [File(f"{video}")]
-        else:
-            attachments = MISSING
-
-        if message:
-            if not message.flags.suppress_embeds:
-                await message.edit(suppress=True)
-            await message.reply(embeds=embeds, file=attachments[0], mention_author=False)
-        elif interaction:
-            await interaction.edit_original_response(content="", embeds=embeds, attachments=attachments)
-
-        for file in post.post_video_files:
-            os.remove(file)
 
     @command(name=COG_STRINGS["instagram_embed_name"], description=COG_STRINGS["instagram_embed_description"])
     @describe(url=COG_STRINGS["instagram_embed_url_describe"])
@@ -233,7 +231,7 @@ class InstagramEmbed(GroupCog, name=COG_STRINGS["instagram_group_name"]):
             await respond_or_followup(message=COG_STRINGS["instagram_warn_invalid_url"], interaction=interaction)
             return
 
-        await self.post_response(url=url, interaction=interaction)
+        await self.request_reply(url=url, interaction=interaction)
 
 
 async def setup(bot: Bot):
